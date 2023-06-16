1ced74bdadad194642af6ade331b9c0746e71d08
ElvisRD <animeamv682@gmail.com>
2023-06-16 02:26:01 -0400

    se agregaron nuevas imagenes para el carrusel

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -12,18 +12,21 @@ export class CarruselComponent {
   loadingImg: boolean = true;
 
   onImageLoad(){
-    console.log("hola");
-    
     this.loadingImg = false;
   }
 
   ngOnInit(){
     let arrayImgs = []
-      
+    
     this.firstSprite = this.imgs.front_default;
     arrayImgs[0] = this.imgs.back_default;
     arrayImgs[1] = this.imgs.front_shiny;
     arrayImgs[2] = this.imgs.back_shiny;
+    arrayImgs[3] = this.imgs.other.dream_world.front_default;
+    arrayImgs[4] = this.imgs.other.home.front_default;
+    arrayImgs[5] = this.imgs.other.home.front_shiny;
+    arrayImgs[6] = this.imgs.other["official-artwork"].front_default;
+    arrayImgs[7] = this.imgs.other["official-artwork"].front_shiny;
 
     this.sprites = arrayImgs.filter((img:string) => img !== null);
   }
